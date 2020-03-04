dabe5151cc43156d78ad7f1a1c61ebb99903742e
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-03-04 09:42:00 +0100

    Mise a jour des Namespace

--- Models/ProduitPanier.cs
+++ Models/ProduitPanier.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace AfpaLunch.Models
+namespace Afpetit.Models
 {
     public class ProduitPanier : ItemPanier
     {

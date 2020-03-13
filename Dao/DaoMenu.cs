3f8a66ce0a91fbb76abcf0322a61ec0b55e67b15
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-03-13 09:23:34 +0100

    ajout de methodes dans doa menu

--- Dao/DaoMenu.cs
+++ Dao/DaoMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Afpetit.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,82 @@ namespace Afpetit.Dao
 {
     public class DaoMenu : DaoAccess
     {
+        public List<Menu> GetMenu()
+        {
+            try
+            {
+                return db.Menus.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        public Menu GetMenuById(int IdMenu)
+        {
+            try
+            {
+                return db.Menus.Find(IdMenu);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public bool ChangeStatut(int IdMenu)
+        {
+            try
+            {
+                Menu menu = this.GetMenuById(IdMenu);
+                if(menu != null)
+                {
+                    menu.Statut = !menu.Statut;
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool CreateMenu(Menu menu)
+        {
+            try
+            {
+                db.Menus.Add(menu);
+                db.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool AddCategorieInMenu(int IdMenu, int IdCategorie)
+        {
+            try
+            {
+                Menu menu = this.GetMenuById(IdMenu);
+                Categorie categorie = db.Categories.Find(IdCategorie);
+                menu.Categories.Add(categorie);
+                db.SaveChanges();
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
 
     }
 }

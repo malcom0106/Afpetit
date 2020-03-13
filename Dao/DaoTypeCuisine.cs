3876b2cb1ab58ea9d68f6b6d74eab4751e5a6019
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-03-13 14:27:30 +0100

    Creation DOA typecuisine

---
+++ Dao/DaoTypeCuisine.cs
@@ -0,0 +1,22 @@
+﻿using Afpetit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Afpetit.Dao
+{
+    public class DaoTypeCuisine : DaoAccess
+    {
+        public List<TypeCuisine> GetTypeCuisines()
+        {
+            try
+            {
+                return db.TypeCuisines.Where(t => t.Statut == true).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
